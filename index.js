01efa63856e6b50c9e76f52a208710667b48e5c0
Francisco Burgoa <franciscojburgoa@gmail.com>
2022-11-09 16:17:39 -0300

    modifique delay

--- index.js
+++ index.js
@@ -64,55 +64,54 @@ function main() {
 
   gsap.from(".hola", {
     duration: 1.5,
-    delay: 0.7,
     opacity: 0,
     x: -600,
     // ease: Expo.easeInOut,
   });
   gsap.from(".welcome__subtitulo", {
     duration: 1.5,
-    delay: 1.1,
+    delay: 0.4,
     opacity: 0,
     y: -600,
     // ease: Expo.easeInOut,
   });
   gsap.from(".rol", {
     duration: 1.5,
-    delay: 0.9,
+    delay: 0.2,
     opacity: 0,
     x: 600,
     // ease: Expo.easeInOut,
   });
   gsap.from(".boton-descargar-CV", {
     duration: 1.5,
-    delay: 1.3,
+    delay: 0.6,
     opacity: 0,
     x: -250,
     ease: "bounce.out",
   });
   gsap.from(".linkedin", {
     duration: 1.5,
-    delay: 1.5,
+    delay: 0.8,
     opacity: 0,
     x: -400,
     // ease: Expo.easeInOut,
   });
   gsap.from(".github", {
     duration: 1.5,
-    delay: 1.6,
+    delay: 0.9,
     opacity: 0,
     x: 400,
     // ease: Expo.easeInOut,
   });
   gsap.from(".imagen-header", {
     duration: 1.5,
-    delay: 1.6,
+    delay: 0.9,
     opacity: 0,
     y: -400,
     // ease: Expo.easeInOut,
   });
   gsap.from(".imagen-header-perfil", {
-    duration: 1.5,
+    duration: 0.8,
     delay: 1.7,
     opacity: 0,
     y: -400,
